7cde6a31b0916d44405b5a9442f8596ba8e38931
AudraHartwell <a.hartwell013@gmail.com>
2023-03-16 18:39:43 -0500

    first

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -11,5 +11,39 @@ export class AppComponent {
   color = 'green';
   height = 0;
   width = 0;
-  message = 'Space shuttle ready for takeoff!';
+  message = 'Space shuttle ready for takeoff!'
+
+handleTakeOff() {
+  let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+  if (result) {
+     this.color = 'blue';
+     this.height = 10000;
+     this.width = 0;
+     this.message = 'Shuttle in flight.';
+  }
+}
+handleLand() {
+  let result = window.confirm('Are you sure the shuttle is ready to land');
+  if (result) {
+     this.color = 'blue';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'Shuttle has landed.';
+  }
+}
+handleAbort() {
+  let result = window.confirm('Are you sure you want to abort mission?');
+  if (result) {
+     this.color = 'red';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'Mission Abored';
+  }
+}
+moveRocket(rocket,direction) {
+  if(direction === 'right') {
+    const movement = parseInt(rocket.style.left) + 10 + 'px';
+  }
+}
+takeOffEnabled: boolean = true;
 }
